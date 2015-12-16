17db0af7c658eef41f570e20a237bf13c589629a
Daniel Costa <costa@vixinet.ch>
2015-12-16 22:41:22 +0200

    Analytics and share

--- deep-linking/boatday.php
+++ deep-linking/boatday.php
@@ -251,10 +251,17 @@
 					</div>
 
 					<div class="share">
-						<!-- <a href="">Let other friends <br/>know about this</a> -->
-					</div>
+						<a href="javascript:fbShare(<?php echo $boatdayid ?>)">Let other friends <br/>know about this BoatDay</a>
+					</div> 
 				</div>
 			</div>
 		</div>
+		<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		  ga('create', 'UA-51849119-1', 'auto');
+		  ga('send', 'pageview');
+		</script>
 	</body>
 </html>
